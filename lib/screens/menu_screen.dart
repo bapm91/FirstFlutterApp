cc7abe10e1021f13196da9376bf42c733f000b68
Kulynych Slava <Kulynych.v.a@gmail.com>
2019-03-03 18:59:49 +0200

    add widgets fnd logic

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -1,11 +1,134 @@
+import 'package:first_flutter_app/screens/game_info_screen.dart';
+import 'package:first_flutter_app/screens/how_to_play_screen.dart';
+import 'package:first_flutter_app/screens/multi_mode_screen.dart';
+import 'package:first_flutter_app/screens/settings_screen.dart';
+import 'package:first_flutter_app/screens/single_mode_screen.dart';
 import 'package:flutter/material.dart';
 
-//Menu with 4 buttons [multi_player, single_player, how_to_play, game_info]
+//Menu with 5 buttons [multi_player, single_player, how_to_play, game_info, settings]
 
 class MenuScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        color: Colors.blue[100],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: ButtonTheme(
+                    minWidth: 200.0,
+                    height: 40.0,
+                    child: RaisedButton(
+                      padding: EdgeInsets.fromLTRB(45, 8, 45, 8),
+                      child: Text('Multiplayer'),
+                      textColor: Colors.white,
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: ButtonTheme(
+                    minWidth: 200.0,
+                    height: 40.0,
+                    child: RaisedButton(
+                      padding: EdgeInsets.fromLTRB(45, 8, 45, 8),
+                      child: Text('Singleplayer'),
+                      textColor: Colors.white,
+                      onPressed: () {
+                        navigateToSingleModeGame(context);
+                      },
+                    )),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                  )),
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: ButtonTheme(
+                    minWidth: 200.0,
+                    height: 40.0,
+                    child: RaisedButton(
+                      padding: EdgeInsets.fromLTRB(45, 8, 45, 8),
+                      child: Text('How to play'),
+                      textColor: Colors.white,
+                      onPressed: () {
+                        navigateToHowToPlay(context);
+                      },
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: ButtonTheme(
+                    minWidth: 200.0,
+                    height: 40.0,
+                    child: RaisedButton(
+                      padding: EdgeInsets.fromLTRB(45, 8, 45, 8),
+                      child: Text('Settings'),
+                      textColor: Colors.white,
+                      onPressed: () {
+                        navigateToSettingsScreen(context);
+                      },
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
+                child: ButtonTheme(
+                    minWidth: 200.0,
+                    height: 40.0,
+                    child: RaisedButton(
+                      padding: EdgeInsets.fromLTRB(45, 8, 45, 8),
+                      child: Text('Game info'),
+                      textColor: Colors.white,
+                      onPressed: () {
+                        navigateToInfo(context);
+                      },
+                    )),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  navigateToMultiModeGame(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MultiModeGameScreen()),
+    );
+  }
+
+  navigateToSingleModeGame(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SingleModeGameScreen()),
+    );
+  }
+
+  navigateToHowToPlay(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => HowToPlay()),
+    );
+  }
+
+  navigateToInfo(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => GameInfo()),
+    );
+  }
+
+  navigateToSettingsScreen(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SettingsScreen()),
+    );
   }
 }
